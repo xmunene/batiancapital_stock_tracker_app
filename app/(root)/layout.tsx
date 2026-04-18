6d387b04c50b134b8f0a353af45a555d9ce0ed30
xmunene <vmunene42@gmail.com>
2026-04-18 13:09:56 +0300

    Nav bar added

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,4 +1,4 @@
-import Header from "@/components/ui/Header"
+import Header from "@/components/ui/header"
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,18 +1,21 @@
-import Image from "next/image"
-import Link from "next/link"
+import Image from "next/image";
+import Link from "next/link";
+import NavItems from "@/components/ui/NavItems";
 
-const Header = () => {
+const header = () => {
   return (
     <header className="sticky top-0 header">
         <div className="container header-wrapper">
             <Link href="/">
-               <Image src="/assets/icons/logo.svg" alt="Logo" width={32} height={32} className="h-8 w-auto cursor-pointer" />
-            
+               <Image src="/assets/icons/logo.svg" alt="Logo" width={140} height={32} className="h-15 w-auto cursor-pointer" />         
             </Link>
+            <nav className="hidden sm:block">
+              <NavItems />
+            </nav>
         </div>
 
     </header>
   )
 }
 
-export default Header
+export default header
